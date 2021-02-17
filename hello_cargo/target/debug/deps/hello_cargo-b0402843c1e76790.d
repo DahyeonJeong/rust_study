49abae88a3106718d6b1eba906a87c4815537efc
Dahyeon Jeong <snoopy809622@gmail.com>
2021-02-17 19:47:22 +0900

    add cargo directory

---
+++ hello_cargo/target/debug/deps/hello_cargo-b0402843c1e76790.d
@@ -0,0 +1,5 @@
+c:\Users\PLAS\Desktop\rust_study\hello_cargo\target\debug\deps\hello_cargo-b0402843c1e76790.rmeta: src\main.rs
+
+c:\Users\PLAS\Desktop\rust_study\hello_cargo\target\debug\deps\hello_cargo-b0402843c1e76790.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\PLAS\Desktop\rust_study\hello_cargo\target\debug\hello_cargo.exe: C:\Users\PLAS\Desktop\rust_study\hello_cargo\src\main.rs
