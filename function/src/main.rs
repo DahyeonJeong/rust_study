f2907a4d25fbd6c13ae7e648fd0ec2ff09d43c92
DahyeonJeong <snoopy809622@gmail.com>
2021-02-22 00:45:25 +0900

    write function project

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    print_x(6);
+
+    print_xy(6, 22);
+
+    println!("{}", return_six());
+}
+
+fn print_x (x: i32) {
+    println!("x의 값: {}", x);
+}
+
+fn print_xy (x: i32, y: i32) {
+    println!("x의 값: {}", x);
+    println!("y의 값: {}", y);
+}
+
+fn return_six() -> i32 {
+    6
 }
