c29688b026dadf5975c4677292f0659b539642df
Dahyeon Jeong <snoopy809622@gmail.com>
2021-02-22 20:48:01 +0900

    save

---
+++ ownership_return/target/debug/deps/ownership_return.d
@@ -0,0 +1,5 @@
+C:\Users\PLAS\Desktop\rust_study\ownership_return\target\debug\deps\ownership_return.exe: src\main.rs
+
+C:\Users\PLAS\Desktop\rust_study\ownership_return\target\debug\deps\ownership_return.d: src\main.rs
+
+src\main.rs:
---
+++ ownership_return/target/rls/debug/deps/ownership_return-b82937559faf5432.d
@@ -0,0 +1,5 @@
+c:\Users\PLAS\Desktop\rust_study\ownership_return\target\rls\debug\deps\ownership_return-b82937559faf5432.rmeta: src\main.rs
+
+c:\Users\PLAS\Desktop\rust_study\ownership_return\target\rls\debug\deps\ownership_return-b82937559faf5432.d: src\main.rs
+
+src\main.rs:
