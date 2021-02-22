0fe2593adb9b380ec0dae18733c4bcfddc976798
DahyeonJeong <snoopy809622@gmail.com>
2021-02-22 12:06:41 +0900

    add loops project

---
+++ loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+/Users/jeongdahyeon/rust_study/loops/target/debug/deps/loops: src/main.rs
+
+/Users/jeongdahyeon/rust_study/loops/target/debug/deps/loops.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/rls/debug/deps/loops-ba133bc238e3f383.d
@@ -0,0 +1,5 @@
+/Users/jeongdahyeon/rust_study/loops/target/rls/debug/deps/loops-ba133bc238e3f383.rmeta: src/main.rs
+
+/Users/jeongdahyeon/rust_study/loops/target/rls/debug/deps/loops-ba133bc238e3f383.d: src/main.rs
+
+src/main.rs:
