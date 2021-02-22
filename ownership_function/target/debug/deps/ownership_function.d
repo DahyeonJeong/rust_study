5bcf42f5015b1f1966c197442efc8cd6185ae78f
Dahyeon Jeong <snoopy809622@gmail.com>
2021-02-22 18:18:49 +0900

    add ownerhip_function project

---
+++ ownership_function/target/debug/deps/ownership_function.d
@@ -0,0 +1,5 @@
+C:\Users\PLAS\Desktop\rust_study\ownership_function\target\debug\deps\ownership_function.exe: src\main.rs
+
+C:\Users\PLAS\Desktop\rust_study\ownership_function\target\debug\deps\ownership_function.d: src\main.rs
+
+src\main.rs:
---
+++ ownership_function/target/debug/ownership_function.d
@@ -0,0 +1,1 @@
+C:\Users\PLAS\Desktop\rust_study\ownership_function\target\debug\ownership_function.exe: C:\Users\PLAS\Desktop\rust_study\ownership_function\src\main.rs
