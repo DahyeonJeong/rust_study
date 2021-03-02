1820e3375b709305c41aa0bb57dee9e2f4147430
dahyeon-j <snoopy809622@gmail.com>
2021-03-02 17:53:42 +0900

    update slice

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes(); // byte 타입으로 변경
+
+    for (i, &item) in bytes.iter().enumerate() { // iter(): 반복자, enumerate(): 튜플(인덱스, 참조) 리턴
+        if item == b' ' {
+            return i;
+        }
+    }
+}
