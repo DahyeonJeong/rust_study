0d1282cd87f2e6ba0fd12a55b50bd03a1dfd2367
dahyeon-j <snoopy809622@gmail.com>
2021-03-02 17:51:13 +0900

    update references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
-    let mut s = String::from("hello");
-    {
-        let r1 = &mut s;
-    }
-    let r2 = &mut s;
+    let reference_to_nothing = dangle();
 
+    let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &s;
+    // println!("{} and {}", r1, r2);
     
+    // {
+    //     let r1 = &mut s;
+    // }
+    // let r2 = &mut s;
+
     change(&mut s);
 
     let len = calculate_length(&s);
 
     println!("'{}'의 길이는 {}입니다.", s, len);
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }
 
 fn calculate_length(s: &String) -> usize {
--- references/target/debug/deps/references.d
+++ references/target/debug/deps/references.d
@@ -1,5 +1,5 @@
-/Users/jeongdahyeon/rust_study/references/target/debug/deps/references: src/main.rs
+C:\Users\PLAS\Desktop\rust_study\references\target\debug\deps\references.exe: src\main.rs
 
-/Users/jeongdahyeon/rust_study/references/target/debug/deps/references.d: src/main.rs
+C:\Users\PLAS\Desktop\rust_study\references\target\debug\deps\references.d: src\main.rs
 
-src/main.rs:
+src\main.rs:
