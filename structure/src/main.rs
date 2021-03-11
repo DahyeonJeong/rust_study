6c30bd8492f495c22151f007c4ddb95ee8e8b8f2
dahyeon-j <snoopy809622@gmail.com>
2021-03-11 16:38:02 +0900

    210311

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -4,12 +4,18 @@ fn main() {
         email: String::from("sdardew@tistory.com"),
         username: String::from("sdardew")
     };
-    
+
     println!("{}: {}", user1.username, user1.email);
 
     user1 = build_user(String::from("sdardew-valley"), String::from("sdardew@mail.com"));
 
     println!("{}: {}", user1.username, user1.email);
+
+    let user2 = User {
+        username: String::from("sdardew"),
+        ..user1
+    };
+
 }
 
 fn build_user(username: String, email: String) -> User {
